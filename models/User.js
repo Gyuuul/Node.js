0b4d8c921dac208cd879fe74cb16d88cc8d62356
Gyuuul <kknd2848@naver.com>
2024-02-27 23:11:37 +0900

    bcrypt 부분 추가

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose= require('mongoose')
+const bcrypt= require('bcrypt')
+// saltRounds: salt가 몇글자인지 지정 > salt 생성 > salt를 이용하여 비번 암호화
+const saltRounds= 10
+const jwt= require('jsonwebtoken')
 
 const userSchema= mongoose.Schema({
     name: {
@@ -32,6 +36,88 @@ const userSchema= mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    var user = this
+
+    // 유저가 비밀번호를 변경하려 할 때
+    if(user.isModified('password')){
+
+    // 비밀번호를 암호화 시킨다.
+    // salt를 generate 할 때, saltRounds 필요
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+        //myPlaintextPassword은 postman에서 투명하게 보인 내 비번
+        // hash는 암호화된 내 비번
+        // bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+        // });
+        bcrypt.hash(user.password , salt , function(err, hash){
+            if(err) return next(err)
+            user.password = hash
+            next()
+        })
+    });
+
+    }else {
+        next()
+    }
+})
+
+// comparePassword메서드 생성
+// userSchema.methods.comparePassword = function(plainPassword, cb){
+
+//     // plainPassword: 1234567, 암호화된 비번: 12fdsf23rugj 이 맞는지
+//     // 확인하려면 plainPassword도 암호화 후  비교해야함
+
+//     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+//         if(err) return cb(err),
+//         // 비번 일치할 경우
+//             cb(null, isMatch)
+//     })
+// }
+
+// generateToken메서드 생성
+// index.js generateToken에 콜백함수 하나만 들어있어서 밑에도 cb만 써준거
+// userSchema.methods.generateToken = function(cb){
+//     var user= this
+
+//     // jsonwebtoken을 이용해서 token을 생성하기
+//     // sign을 이용해서 token을 생성함
+//     jwt.sign(user._id.toHexString(), 'secretToken')
+
+//     var token= user._id + 'secretToken'
+
+//     user.token= token
+//     user.save(function(err, user){
+//         if(err) return cb(err),
+//             cb(null, user)
+//     })
+// }
+
+userSchema.methods.comparePassword = function (plainPassword) {
+    // plainpassword와 db에 암호화된 비밀번호가 같은지 확인
+    const result = bcrypt.compare(plainPassword, this.password);
+    return result;
+  };
+  
+  
+  userSchema.methods.generateToken = async function (cb) {
+    var user = this;
+    // jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), "secretToken");
+    // user._id(db의 _id) + secreToken = token
+    // token으로 user를 판별할 수 있다.
+    // user.token = token;
+    user.token = token;
+  
+    try {
+      const savedUser = await user.save();
+      return user;
+    } catch (err) {
+      return err;
+    }
+  };
+
 
 // model로 schema 감싸줌
 const User= mongoose.model('User', userSchema)
