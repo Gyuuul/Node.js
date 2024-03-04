ec8333f73d1979524eec7dd2a30540bdf35a0ec1
Gyuuul <kknd2848@naver.com>
2024-03-04 16:56:23 +0900

    redux 추가

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import {thunk} from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware= applyMiddleware(promiseMiddleware, thunk)(createStore);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__())}>
     <App />
-  </>
+  </Provider>
 );
 
 reportWebVitals();
