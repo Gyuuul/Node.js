6647c9475355683a8ee853b3aad2013180a8d14c
Gyuuul <kknd2848@naver.com>
2024-03-06 01:37:57 +0900

    route에 hoc 적용

--- client/src/route.js
+++ client/src/route.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-
 import LandingPage from './components/views/LandingPage/LandingPage';
 // import Chat from './Chat/Chat';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth';
+
 export default function route() {
+    const AuthLandingPage = Auth(LandingPage, null);
+    const AuthLoginPage = Auth(LoginPage, false);
+    const AuthRegisterPage = Auth(RegisterPage, false);
+
     return (
         <>
             <Router>
                 <Routes>
-                    <Route path="/" element={<LandingPage/>} />
-                    {/* <Route path='/chat' element={<Chat/>} /> */}
-                    <Route path="/login" element={<LoginPage/>} />
-                    <Route path="/register" element={<RegisterPage/>} />
+                    <Route path="/" element={ <AuthLandingPage /> } />
+                    <Route path="/login" element={ <AuthLoginPage /> }/>
+                    <Route path="/register" element={ <AuthRegisterPage /> } />
                 </Routes>
             </Router>
         </>
