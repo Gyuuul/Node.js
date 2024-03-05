31bf03ad7540edfb2a87feeeda7cce29cad7726a
Gyuuul <kknd2848@naver.com>
2024-03-06 01:38:23 +0900

    user_action auth 추가

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER, REGISTER_USER } from './types'
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from './types'
 
 // login 페이지에서 받아온 정보를 파라미터에 넣어줌
 export async function loginUser(dataToSubmit){
@@ -18,6 +18,17 @@ export async function registerUser(dataToSubmit){
 
     return{
         type: REGISTER_USER,
+        payload: request,
+    }
+}
+
+export async function auth(){
+    // get method는 body부분이 필요 없음
+    const request= await axios.get('/api/users/auth')
+    .then(response=> response.data)
+
+    return{
+        type: AUTH_USER,
         payload: request,
     }
 }
