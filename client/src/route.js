71de5b04b65c38c80497d93145f05b61f3da6f5b
Gyuuul <kknd2848@naver.com>
2024-03-05 17:10:46 +0900

    route 경로 추가

--- client/src/route.js
+++ client/src/route.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LandingPage from './components/views/LandingPage/LandingPage';
-
+// import Chat from './Chat/Chat';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 export default function route() {
     return (
         <>
             <Router>
                 <Routes>
-                    <Route path='/' element={<LandingPage/>} />
+                    <Route path="/" element={<LandingPage/>} />
+                    {/* <Route path='/chat' element={<Chat/>} /> */}
+                    <Route path="/login" element={<LoginPage/>} />
+                    <Route path="/register" element={<RegisterPage/>} />
                 </Routes>
             </Router>
         </>
