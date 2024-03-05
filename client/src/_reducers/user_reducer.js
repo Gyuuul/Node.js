cffce71cbf9caaf012cd60e0094b5f57dda2b136
Gyuuul <kknd2848@naver.com>
2024-03-06 01:38:37 +0900

    reducer auth 추가

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER, REGISTER_USER } from "../_actions/types";
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "../_actions/types";
 
 // reducer의 인자는 (previousState, action)
 export default function(state= {}, action){
@@ -9,6 +9,12 @@ export default function(state= {}, action){
 
         case REGISTER_USER:
             return{...state, registerSuccess: action.payload}
+        break;
+
+        case AUTH_USER:
+            // server auth router에 유저의 정보가 모두 담겨 있음
+            // 그 유저 정보가 action.payload에 담겨있는 것
+            return{...state, userData: action.payload}
         break;
 
         default:
