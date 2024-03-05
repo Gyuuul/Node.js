2e2b178ead6c0614f4e97c6efb699345ea561508
Gyuuul <kknd2848@naver.com>
2024-03-05 23:55:07 +0900

    user_action register 추가

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER } from './types'
+import { LOGIN_USER, REGISTER_USER } from './types'
 
 // login 페이지에서 받아온 정보를 파라미터에 넣어줌
 export async function loginUser(dataToSubmit){
@@ -10,5 +10,14 @@ export async function loginUser(dataToSubmit){
         type: LOGIN_USER,
         payload: request
     }
+}
 
+export async function registerUser(dataToSubmit){
+    const request= await axios.post('/api/users/register',dataToSubmit)
+    .then(response=> response.data)
+
+    return{
+        type: REGISTER_USER,
+        payload: request,
+    }
 }
