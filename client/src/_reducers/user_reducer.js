92f3515fc9308787ab099b4c012bfa91d168a303
Gyuuul <kknd2848@naver.com>
2024-03-05 23:55:39 +0900

    reducer register 추가

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,10 +1,14 @@
-import { LOGIN_USER } from "../_actions/types";
+import { LOGIN_USER, REGISTER_USER } from "../_actions/types";
 
 // reducer의 인자는 (previousState, action)
 export default function(state= {}, action){
     switch (action.type){
         case LOGIN_USER:
             return{...state, loginSuccess: action.payload}
+        break;
+
+        case REGISTER_USER:
+            return{...state, registerSuccess: action.payload}
         break;
 
         default:
